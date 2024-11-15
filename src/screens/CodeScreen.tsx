ba0d24d4e9316dd22ed6c4c93bdc95728cfc711c
Ayomide Omotosho <aomotosho4@gmail.com>
2024-11-14 21:17:55 -0600

    fix: input hidden by keyboard

--- src/screens/CodeScreen.tsx
+++ src/screens/CodeScreen.tsx
@@ -6,6 +6,9 @@ import {
   Pressable,
   ImageBackground,
   Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
 } from "react-native";
 
 export default function CodeScreen({ navigation }: any) {
@@ -14,38 +17,46 @@ export default function CodeScreen({ navigation }: any) {
       source={require("../../assets/bg-blob.png")}
       style={styles.backgroundImage}
     >
-      <View style={styles.mainContainer}>
-        <View style={{ padding: 40, paddingTop: 60 }}>
-          <Text style={{ fontSize: 20, marginBottom: 30 }}>
-            Which Dish Will End on Top? Join the Fun and Find Out!
-          </Text>
-          <Image
-            style={styles.img}
-            source={require("../../assets/blob.png")}
-            blurRadius={20}
-          />
-          <View style={{ marginBottom: 100 }}>
-            <Text style={{ fontSize: 60 }}>Enter</Text>
-            <Text style={{ fontSize: 60 }}>The</Text>
-            <Text style={{ fontSize: 60 }}>Party</Text>
-            <Text style={{ fontSize: 60 }}>Code</Text>
-          </View>
-          <View>
-            <TextInput
-              style={[styles.input, styles.shadowProp]}
-              placeholder="Enter Code Here"
-            ></TextInput>
-            <Pressable
-              style={styles.button}
-              onPress={() => navigation.navigate("Welcome")}
-            >
-              <Text style={{ fontWeight: "500", color: "#fff", fontSize: 16 }}>
-                Submit
-              </Text>
-            </Pressable>
+      <KeyboardAvoidingView
+        style={styles.mainContainer}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View>
+          <View style={{ padding: 40, paddingTop: 60 }}>
+            <Text style={{ fontSize: 20, marginBottom: 30 }}>
+              Which Dish Will End on Top? Join the Fun and Find Out!
+            </Text>
+            <Image
+              style={styles.img}
+              source={require("../../assets/blob.png")}
+              blurRadius={20}
+            />
+            <View style={{ marginBottom: 100 }}>
+              <Text style={{ fontSize: 60 }}>Enter</Text>
+              <Text style={{ fontSize: 60 }}>The</Text>
+              <Text style={{ fontSize: 60 }}>Party</Text>
+              <Text style={{ fontSize: 60 }}>Code</Text>
+            </View>
+            <View>
+              <TextInput
+                style={[styles.input, styles.shadowProp]}
+                placeholder="Enter Code Here"
+              ></TextInput>
+              <Pressable
+                style={styles.button}
+                onPress={() => navigation.navigate("Welcome")}
+              >
+                <Text
+                  style={{ fontWeight: "500", color: "#fff", fontSize: 16 }}
+                >
+                  Submit
+                </Text>
+              </Pressable>
+            </View>
           </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </ImageBackground>
   );
 }
