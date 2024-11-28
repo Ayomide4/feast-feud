c719abf2b7359403ebd77c7796d2055db9b76579
Ayomide Omotosho <aomotosho4@gmail.com>
2024-11-28 17:15:56 -0600

    feat: add galler/camera to modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, Alert, Image } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import UpArrow from "../../assets/svg/up-arrow";
 import { secondary, text } from "../constants/colors";
@@ -6,21 +6,77 @@ import { Dropdown } from "react-native-element-dropdown";
 import Camera from "../../assets/svg/camera";
 import Gallery from "../../assets/svg/gallery";
 import Trash from "../../assets/svg/trash";
-import React, { SetStateAction } from "react";
+import React, { SetStateAction, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 interface Props {
   setIsModalOpen: React.Dispatch<SetStateAction<boolean>>;
 }
 
+interface Dish {
+  dishName: string;
+  category: string;
+  dishImage: string;
+}
+
 //TODO:
-//- add photo gallery stuff
+//- add feedback on button press
+//- add preview of card?
+//- add dish  to firebase
 
 export default function Modal({ setIsModalOpen }: Props) {
+  const [dish, setDish] = useState<Dish>({
+    dishName: "",
+    category: "",
+    dishImage: "",
+  });
   const data = [
     { label: "Main Dish", value: "Main Dish" },
     { label: "Side/Appetizer", value: "Side/Appetizer" },
     { label: "Dessert", value: "Dessert" },
   ];
+
+  const handleChangeText = (property: keyof Dish, text: string) => {
+    setDish((prev: Dish) => ({
+      ...prev,
+      [property]: text,
+    }));
+  };
+
+  const handleOpenCamera = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission Denied",
+        "Camera permission is required to take a photo.",
+      );
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      handleChangeText("dishImage", result.assets[0].uri);
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      handleChangeText("dishImage", result.assets[0].uri);
+    }
+  };
+
   return (
     <View
       style={{
@@ -38,7 +94,13 @@ export default function Modal({ setIsModalOpen }: Props) {
               flexDirection: "column",
             }}
           >
-            <View style={{ marginVertical: 20 }}>
+            <Pressable
+              style={styles.backBtn}
+              onPress={() => setIsModalOpen(false)}
+            >
+              <UpArrow rotationDeg={270} color={text} width={30} height={30} />
+            </Pressable>
+            <View style={{ marginVertical: 10 }}>
               <Text style={{ fontSize: 18, fontWeight: "bold" }}>
                 Add Your Dish
               </Text>
@@ -59,6 +121,9 @@ export default function Modal({ setIsModalOpen }: Props) {
                 style={styles.input}
                 placeholder="Enter dish name..."
                 placeholderTextColor="#000"
+                onChangeText={(text) => handleChangeText("dishName", text)}
+                value={dish.dishName}
+                returnKeyType="done"
               />
             </View>
             <View style={{ marginBottom: 20 }}>
@@ -74,18 +139,18 @@ export default function Modal({ setIsModalOpen }: Props) {
               <Dropdown
                 style={styles.dropdown}
                 containerStyle={styles.inputSearch}
+                itemContainerStyle={{
+                  backgroundColor: "#E8E8FB",
+                }}
+                activeColor={secondary}
                 maxHeight={300}
                 labelField="label"
                 valueField="value"
                 data={data}
                 placeholder="Select your category"
                 placeholderStyle={styles.placeholderStyle}
-                onChange={function (item: {
-                  label: string;
-                  value: string;
-                }): void {
-                  throw new Error("Function not implemented.");
-                }}
+                value={dish.category}
+                onChange={(item) => handleChangeText("category", item.value)}
               />
             </View>
             <View style={{ marginBottom: 50 }}>
@@ -98,19 +163,32 @@ export default function Modal({ setIsModalOpen }: Props) {
                 style={{
                   flexDirection: "row",
                   alignItems: "center",
-                  justifyContent: "space-between",
-                  width: "80%",
+                  justifyContent: "space-evenly",
                 }}
               >
-                <Camera width={50} height={50} />
-                <Gallery width={40} height={40} />
-                <Trash width={50} height={50} />
+                <Pressable
+                  style={styles.btnBackground}
+                  onPress={handleOpenCamera}
+                >
+                  <Camera width={50} height={50} />
+                </Pressable>
+
+                <Pressable style={styles.btnBackground} onPress={pickImage}>
+                  <Gallery width={40} height={40} />
+                </Pressable>
+                <Pressable
+                  style={styles.btnBackground}
+                  onPress={() => handleChangeText("dishImage", "")}
+                >
+                  <Trash width={50} height={50} />
+                </Pressable>
               </View>
             </View>
+
             <View>
               <Pressable
                 style={styles.addBtn}
-                onPress={() => console.log("created")}
+                onPress={() => Alert.alert(JSON.stringify(dish))}
                 accessibilityLabel="Press this to add your dish to the items in the party"
               >
                 <Text style={{ color: text }}>Add Dish</Text>
@@ -191,5 +269,21 @@ const styles = StyleSheet.create({
   },
   inputSearch: {
     backgroundColor: "#E8E8FB",
+  },
+  backBtn: {
+    borderRadius: 100,
+    backgroundColor: secondary,
+    width: 40,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnBackground: {
+    borderRadius: 10,
+    backgroundColor: "#D8D8EC",
+    width: 60,
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
