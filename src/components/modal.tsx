4e16752a1e3fb8aebf832a8bfe686382d3ddbde6
Ayomide Omotosho <aomotosho4@gmail.com>
2024-11-29 15:49:25 -0600

    fix: modal zIndex

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -254,6 +254,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     top: 0,
+    zIndex: 2,
   },
 
   modalContainer: {
@@ -264,6 +265,8 @@ const styles = StyleSheet.create({
     opacity: 1,
     top: 100,
     borderRadius: 10,
+
+    zIndex: 3,
   },
 
   formContainer: {
